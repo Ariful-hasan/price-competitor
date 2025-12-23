673adb20c444305338210b5028e8a7156ec244b3
Ariful Hasan <arifhasan.mbstu@gmail.com>
2025-12-23 15:57:24 +0100

    add ratelimiter to all routes like auth+api+public

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,23 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('throttle:auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+    });
+    
 
-    Route::middleware('auth:api')->group(function () {
+    Route::middleware(['auth:api', 'throttle:api'])->group(function () {
         Route::post('profile', [AuthController::class, 'profile']);
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
     });
 });
 
-// Get all products (paginated)
-Route::get('products', [ProductController::class, 'index']);
+Route::middleware(['auth:api', 'throttle:api'])->group(function () {
+    // Get all products (paginated)
+    Route::get('products', [ProductController::class, 'index']);
 
-// Get a single product by ID
-Route::get('products/{product_id}', [ProductController::class, 'show']);
+    // Get a single product by ID
+    Route::get('products/{product_id}', [ProductController::class, 'show']);
+});
